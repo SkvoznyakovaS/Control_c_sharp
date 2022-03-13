9f5c841308b7a7601962cebe4c2537396b28a652
Svetlana_Skvoznyakova <s.skvoznyakova@mail.ru>
2022-03-13 16:25:02 +0300

    add создали новый массив с заданными усовиями

--- csharp/control/Program.cs
+++ csharp/control/Program.cs
@@ -1,2 +1,8 @@
-﻿string[] array = {"1234", "1567", "-2", "comp uter science"};
+﻿string[] array = {"1234", "1567", "-2", "comp uter science", "дом"};
 //создали массив
+Console.WriteLine(string.Join(" ", array));
+//вывели на печать массив
+string[] new_array = Array.FindAll(array, a=> a.Length <=3);
+//создали новый массив
+Console.WriteLine(string.Join(" ", new_array));
+//вывели на печать новый массив
